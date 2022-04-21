f9125566e89e6cf90772c5d11a21e3e3445017c7
MargaretKerubo <98818039+MargaretKerubo@users.noreply.github.com>
2022-04-21 20:54:36 +0300

    Updates

---
+++ precache-manifest.4a631f7009a052ca3cee8df683d744da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "849298137630bce36dba",
+    "url": "/SideHustle-React.js/static/css/main.d78c25e7.chunk.css"
+  },
+  {
+    "revision": "849298137630bce36dba",
+    "url": "/SideHustle-React.js/static/js/main.84929813.chunk.js"
+  },
+  {
+    "revision": "abad929eae97f4972f42",
+    "url": "/SideHustle-React.js/static/css/1.a80ee736.chunk.css"
+  },
+  {
+    "revision": "abad929eae97f4972f42",
+    "url": "/SideHustle-React.js/static/js/1.abad929e.chunk.js"
+  },
+  {
+    "revision": "662659f8ba63e3c6ad0f",
+    "url": "/SideHustle-React.js/static/js/runtime~main.662659f8.js"
+  },
+  {
+    "revision": "cd7c7ec90db0434442bd3ac52582f2d4",
+    "url": "/SideHustle-React.js/static/media/profile.cd7c7ec9.jpg"
+  },
+  {
+    "revision": "1d95e8bafaf3f5aaf02d31e475f546db",
+    "url": "/SideHustle-React.js/index.html"
+  }
+];
